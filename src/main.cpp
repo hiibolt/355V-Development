caf87840b89d7dc218721768c8a964ee60e4e3a9
John White <91273156+BoltR6@users.noreply.github.com>
2022-10-28 23:12:30 -0500

    Two new types of drive, the ability to change them, and motor correction

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <math.h>
 
 /**
  * A callback function for LLEMU's center button.
@@ -62,6 +63,19 @@ void competition_initialize() {}
 void autonomous() {}
 
 /**
+	Accepts a pointer to a signed char voltage (-127-127)
+		[Coincidentally the max/min voltage the motors accept]
+	Sets that voltage to -30 or 30 respectively if it's close to zero
+	This fixes non-functioning low power when it should be moving
+**/
+void removeLowPowerIssues(signed char* value){
+	if(*value < 30 && *value > 5){
+		*value =  30;
+	}else if(*value > -30 && *value < -5){
+		*value =  -30;
+	}
+}
+/**
  * Runs the operator control code. This function will be started in its own task
  * with the default priority and stack size whenever the robot is enabled via
  * the Field Management System or the VEX Competition Switch in the operator
@@ -82,24 +96,84 @@ void opcontrol() {
 	pros::Motor right_mtr1(8, false);
 	pros::Motor right_mtr2(9, false);
 	pros::Motor right_mtr3(10, false);
+	unsigned char driveType = 0;
+	/**
+		0: Tank
+		1: Split Arcade
+	**/
 
 	printf("Initialized!");
 	while (true) {
-		//pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		//                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		//                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+		//Switching drive types
+		if(master.get_digital_new_press(DIGITAL_UP)){
+			driveType ++;
+		}
 
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+		//Will be printed later
+		std::string info = "";
+		switch(driveType){
+			case 0: {
+				//Tank
+				info = "Tank";
+				signed char left = master.get_analog(ANALOG_LEFT_Y);
+				signed char right = master.get_analog(ANALOG_RIGHT_Y);
+				removeLowPowerIssues(&left);
+				removeLowPowerIssues(&right);
 
-		left_mtr1.move(left);
-		left_mtr2.move(left);
-		left_mtr3.move(left);
-		right_mtr1.move(right);
-		right_mtr2.move(right);
-		right_mtr3.move(right);
+				left_mtr1.move(left);
+				left_mtr2.move(left);
+				left_mtr3.move(left);
+				right_mtr1.move(right);
+				right_mtr2.move(right);
+				right_mtr3.move(right);
+				break;
+			}
+			case 1: {
+				//Split Arcade (High Precision, Half Speed)
+				info = "Split Arcade [HP]";
+				signed char left = (master.get_analog(ANALOG_LEFT_Y) + master.get_analog(ANALOG_RIGHT_X)) / 2;
+				signed char right = (master.get_analog(ANALOG_LEFT_Y) - master.get_analog(ANALOG_RIGHT_X)) / 2;
+				removeLowPowerIssues(&left);
+				removeLowPowerIssues(&right);
 
-		pros::lcd::set_text(0,std::to_string(left) + " | " + std::to_string(right));
-		pros::delay(20);
+				left_mtr1.move(left);
+				left_mtr2.move(left);
+				left_mtr3.move(left);
+				right_mtr1.move(right);
+				right_mtr2.move(right);
+				right_mtr3.move(right);
+				pros::lcd::set_text(0,std::to_string(left) + " | " + std::to_string(right) + " | " + std::to_string(driveType));
+
+				break;
+			}
+			case 2: {
+				//Split Arcade (High Speed, Half Precision)
+				info = "Split Arcade [HS]";
+				signed char left = (master.get_analog(ANALOG_LEFT_Y) + master.get_analog(ANALOG_RIGHT_X)) / 2;
+				signed char right = (master.get_analog(ANALOG_LEFT_Y) - master.get_analog(ANALOG_RIGHT_X)) / 2;
+				signed char up = (master.get_analog(ANALOG_LEFT_Y) * (1 - (127 / abs(master.get_analog(ANALOG_LEFT_X)))) ) / 2;
+				removeLowPowerIssues(&left);
+				removeLowPowerIssues(&right);
+
+				left_mtr1.move(left + up);
+				left_mtr2.move(left + up);
+				left_mtr3.move(left + up);
+				right_mtr1.move(right + up);
+				right_mtr2.move(right + up);
+				right_mtr3.move(right + up);
+				pros::lcd::set_text(0,std::to_string(left) + " | " + std::to_string(right) + " | " + std::to_string(driveType));
+
+				break;
+			}
+			default:
+				driveType = 0;
+		}
+
+		//Display padded information, otherwise information ghosts on screen
+		info.insert(info.end(),18 - info.size(), ' ');
+		master.set_text(0,0,info);
+
+		//Delay to prevent code overflow
+		pros::delay(5);
 	}
 }
