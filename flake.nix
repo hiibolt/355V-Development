21359c918aae78b3a4564f0bfc4c02082e21b9e9
BoltR6 <johnwallacewhite@gmail.com>
2023-03-23 09:51:33 -0500

    Fixed flake

--- flake.nix
+++ flake.nix
@@ -1,19 +1,29 @@
 {
+  description = "Development environment with GCC and PROS";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
   outputs = { self, nixpkgs }: 
     let
+      system = "x86_64-linux";
+      
       pkgs = import nixpkgs { 
-        system = "x86_64-linux";
+        inherit system;
       };
+
+      python-package-list = p: with p; [
+        pip
+      ];
+      pros-python = pkgs.python310.withPackages python-package-list;
     in
     {
     devShells.x86_64-linux.default = with pkgs; mkShell {
-      buildInputs = [ python310 gcc-arm-embedded ];
+      buildInputs = [ pros-python gcc-arm-embedded ];
       shellHook = 
         ''
-        source enviro/bin/activate
+        python -m venv .venv
+        source .venv/bin/activate
+        pip install pros-cli
         '';
       
     };
